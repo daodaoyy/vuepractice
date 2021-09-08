baedf8a8b86ba6883ed655977e896732c21edf67
daoyan.ji <daoyan.ji@luckincoffee.com>
2021-09-08 16:32:49 +0800

    feat(vuepractice)闭包练习

--- client/admin/pages/home/index.vue
+++ client/admin/pages/home/index.vue
@@ -1,10 +1,64 @@
 <template>
-  <div>
-    首页
+  <div class="home">
+    <button @click="clickClosure">tm的闭包</button>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      clickClosure() {
+        // 5->5,5,5,5,5
+        // for (var i = 0; i < 5; i++) {
+        //   setTimeout(function() {
+        //     console.log(new Date, i);
+        //   }, 1000);
+        // }
+        // console.log(new Date, i);
+
+        // 5->0,1,2,3,4
+        // for (var i = 0; i < 5; i++) {
+        //   (function(j) {
+        //     setTimeout(function() {
+        //       console.log(new Date(), j)
+        //     }, 1000)
+        //   })(i)
+        // }
+        // console.log(new Date, i);
+
+        // 5->0,1,2,3,4
+        // for (var i = 0; i < 5; i++) {   
+        //   setTimeout(function(j) {
+        //     console.log(new Date(), j)
+        //   }, 1000, i)
+        //   }
+        // console.log(new Date, i);
+
+        // 报错->0,1,2,3,4
+        for (let i = 0; i < 5; i++) {
+          setTimeout(function() {
+            console.log(new Date(), i)
+          }, 1000)
+        }
+        // console.log(new Date, i);
+      }
+    }
+  };
 </script>
+
+<style lang="less" scoped>
+.home {
+  margin: auto;
+  padding: 30px;
+  button {
+    width: 200px;
+    height: 60px;
+  }
+}
+</style>
 
